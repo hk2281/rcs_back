57e25f843fae8efa69e18357fdffb25d812e72a8
Egor Kondrashov <e.kondr01@gmail.com>
2021-10-14 16:16:48 +0300

    feat: rm fail test and  add ci/cd on worklow_dispatch

--- rcs_back/containers_app/tests/test_models.py
+++ rcs_back/containers_app/tests/test_models.py
@@ -233,6 +233,3 @@ class MassRuleIgnoreReportsTests(TestCase):
         self.assertTrue(self.office_container.is_full())
         self.assertTrue(self.building.meets_mass_takeout_condition())
         self.assertTrue(self.building.needs_takeout())
-
-    def test_fail(self):
-        self.assertTrue(False)
