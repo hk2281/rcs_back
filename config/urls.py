a85b55b6a64fdf590719513d77668fd38bb2f62f
Egor Kondrashov <e.kondr01@gmail.com>
2021-11-01 15:50:57 +0300

    feat: add drf_spectacular

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (SpectacularAPIView,
+                                   SpectacularSwaggerView,
+                                   SpectacularRedocView)
 
-from rcs_back.containers_app.views import (BuildingListView, ContainerCountView,
+from rcs_back.containers_app.views import (BuildingListView,
+                                           ContainerCountView,
                                            PublicFeedbackView,
                                            BuildingPartView,
                                            FullContainerReportView)
@@ -41,3 +45,14 @@ urlpatterns = [
 if bool(settings.DEBUG):
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        path('api/schema/',
+             SpectacularAPIView.as_view(),
+             name='schema'),
+        path('api/schema/swagger-ui/',
+             SpectacularSwaggerView.as_view(url_name='schema'),
+             name='swagger-ui'),
+        path('api/schema/redoc/',
+             SpectacularRedocView.as_view(url_name='schema'),
+             name='redoc'),
+    ]
